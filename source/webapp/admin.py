bf6c03ac831b46bfbb3dac3ddde028f969d6151f
Malik <imenov-a@mail.ru>
2020-07-11 22:51:10 +0600

    Создал базу, Зарегистрировал модель в админ-панели. Добавил 6 записей, сохранил в фикстуре

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Book
 
-# Register your models here.
+
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'name', 'text', 'status']
+#     list_filter = ['name', 'status']
+#     fields = ['name', 'email', 'text', 'status', 'created_at', 'updated_at']
+#     readonly_fields = ['created_at', 'updated_at']
+#
+
+
+
+admin.site.register(Book)
+# admin.site.register(BookAdmin)
+
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_ACTIVE = "active"
+STATUS_CHOICES = (
+    (STATUS_ACTIVE, "Активно"),
+    ("blocked", "Заблокировано")
+)
+
+
+
+class Book(models.Model):
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    email = models.EmailField(max_length=100, verbose_name='Email')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    status = models.CharField(max_length=40, null=False, blank=False, default=STATUS_ACTIVE,
+                              choices=STATUS_CHOICES, verbose_name='Статус')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+
